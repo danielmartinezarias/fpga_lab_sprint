88f374f2a29f4df44beba87f82fffcb84d53f986
danielmartinezarias <danielmartinezarias@gmail.com>
2026-01-09 12:18:54 +0100

    startup_bitsteam: path fix

--- bitstreams/startup_bitstream.sh
+++ bitstreams/startup_bitstream.sh
@@ -1,8 +1,8 @@
-sudo chmod +x /home/petalinux/bitstreams/fpga-bit-to-bin.py
-./home/petalinux/bitstreams/fpga-bit-to-bin.py -f /home/petalinux/bitstreams/des
-sudo fpgautil -o /home/petalinux/bitstreams/pl.dtbo
-sudo fpgautil -b /home/petalinux/bitstreams/BITSTREAM_FILE.bit.bin -f Full
-cd /home/petalinux/python
-export FPGA_MODEL='ZedBoard2'
+sudo chmod +x /home/petalinux/fpga_lab_sprint/bitstreams/fpga-bit-to-bin.py
+./home/petalinux/fpga_lab_sprint/bitstreams/fpga-bit-to-bin.py -f /home/petalinux/bitstreams/des
+sudo fpgautil -o /home/petalinux/fpga_lab_sprint/bitstreams/pl.dtbo
+sudo fpgautil -b /home/petalinux/fpga_lab_sprint/bitstreams/BITSTREAM_FILE.bit.bin -f Full
+cd /home/petalinux/fpga_lab_sprint/python
+export FPGA_MODEL='ZedBoard'
 sudo touch "FPGA_MODEL=$FPGA_MODEL" >> /etc/environment
 sudo -E python3 scripts/version.py
