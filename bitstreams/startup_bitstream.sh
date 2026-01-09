b5812544c32086afde4ceac232c4e23cbf636f57
danielmartinezarias <danielmartinezarias@gmail.com>
2026-01-09 12:22:09 +0100

    startup_bitsteam: fix typo

--- bitstreams/startup_bitstream.sh
+++ bitstreams/startup_bitstream.sh
@@ -1,5 +1,5 @@
 sudo chmod +x /home/petalinux/fpga_lab_sprint/bitstreams/fpga-bit-to-bin.py
-./home/petalinux/fpga_lab_sprint/bitstreams/fpga-bit-to-bin.py -f /home/petalinux/bitstreams/des
+./home/petalinux/fpga_lab_sprint/bitstreams/fpga-bit-to-bin.py -f /home/petalinux/fpga_lab_sprint/bitstreams/design_1_wrapper.bit /home/petalinux/fpga_lab_sprint/bitstreams/design_1_wrapper.bit.bin
 sudo fpgautil -o /home/petalinux/fpga_lab_sprint/bitstreams/pl.dtbo
 sudo fpgautil -b /home/petalinux/fpga_lab_sprint/bitstreams/BITSTREAM_FILE.bit.bin -f Full
 cd /home/petalinux/fpga_lab_sprint/python
